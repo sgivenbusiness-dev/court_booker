8382a2f4188a5ca58e138817192793803d8c79c4
Sean Given <sgive54@wgu.edu>
2026-06-23 22:39:43 -0400

    Add club hours booking validation.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 court = {
-    "name": "court1",
+    "name": "Court 1",
     "surface": "hard",
     "indoor": False
 }
@@ -13,7 +13,6 @@ member = {
     "name": "John Smith",
     "user_type": "member"
 }
-
 
 OPEN_TIME = 8 * 60
 CLOSE_TIME = 20 * 60
@@ -30,5 +29,19 @@ reservation = {
 def is_valid_duration(duration):
     return duration in ALLOWED_DURATIONS
 
-print(is_valid_duration(45)) # False
-print(is_valid_duration(60)) # True
+def is_within_club_hours (start_time, duration):
+    end_time = start_time + duration
+
+    if start_time < OPEN_TIME:
+        return False
+    
+    if end_time > CLOSE_TIME:
+        return False
+    
+
+    return True
+
+print(is_within_club_hours(10 * 60, 60))          # True
+print(is_within_club_hours(7 * 60 + 30, 60))     # False
+print(is_within_club_hours(19 * 60 + 30, 60))    # False
+print(is_within_club_hours(18 * 60, 120))        # True
