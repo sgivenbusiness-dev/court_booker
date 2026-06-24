201156cba67e2a95d405e7ef89f7cd225037043f
Sean Given <sgive54@wgu.edu>
2026-06-24 00:01:09 -0400

    Add booking conflict check.

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ CLOSE_TIME = 20 * 60
 
 ALLOWED_DURATIONS = [30, 60, 90, 120]
 
+
+
+
 reservation = {
     "user": member,
     "court": court,
@@ -26,10 +29,24 @@ reservation = {
     "duration": 60,
 }
 
+new_reservation = {
+    "user": coach,
+    "court": court,
+    "start_time": 10 * 60 + 30,
+    "duration": 60,
+}
+
+bookings = [reservation]
+
+### rule check functions
+
+
 def is_valid_duration(duration):
     return duration in ALLOWED_DURATIONS
 
-def is_within_club_hours (start_time, duration):
+
+
+def is_within_club_hours (start_time, duration):   #makes sure reservation is within
     end_time = start_time + duration
 
     if start_time < OPEN_TIME:
@@ -41,7 +58,47 @@ def is_within_club_hours (start_time, duration):
 
     return True
 
-print(is_within_club_hours(10 * 60, 60))          # True
-print(is_within_club_hours(7 * 60 + 30, 60))     # False
-print(is_within_club_hours(19 * 60 + 30, 60))    # False
-print(is_within_club_hours(18 * 60, 120))        # True
+
+def bookings_overlap(new_booking, existing_booking):   #returns true or false
+    new_start = new_booking["start_time"]
+    new_end = new_start + new_booking["duration"]
+
+    existing_start = existing_booking["start_time"]
+    existing_end = existing_start + existing_booking["duration"]
+
+    return new_start < existing_end and new_end > existing_start
+
+
+def has_booking_conflict(new_booking, bookings):       #boolean
+    for existing_booking in bookings:
+        if bookings_overlap(new_booking, existing_booking):
+            return True
+        
+    return False
+
+            
+selected_user = member
+
+start_hour = int(input("Start hour: "))
+start_minute = int(input("Start minute: "))
+duration = int(input("Duration: "))
+
+start_time = start_hour * 60 + start_minute
+
+new_reservation = {
+    "user": selected_user,
+    "court": court,
+    "start_time": start_time,
+    "duration": duration,
+}
+
+if not is_valid_duration(duration):
+    print("Invalid duration.")
+elif not is_within_club_hours(start_time, duration):
+    print("Reservation must be between 8:00 AM and 8:00 PM.")
+elif has_booking_conflict(new_reservation, bookings):
+    print("Booking conflict. This court is already reserved at that time.")
+else:
+    bookings.append(new_reservation)
+    print("Reservation created.")
+
