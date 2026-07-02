30c426f543cbc6cd8d1dd4ab5cd7c15f67d844f8
Sean Given <sgive54@wgu.edu>
2026-07-01 20:10:14 -0400

    added bookings, courts, and users. added load_bookings and find_user_by_club_number

---
+++ courts.py
@@ -0,0 +1,22 @@
+courts = [
+    {
+        "name": "Outdoor Court 1",
+        "surface": "hard",
+        "indoor": False
+    },
+    {
+        "name": "Outdoor Court 2",
+        "surface": "hard",
+        "indoor": False
+    },
+    {
+        "name": "Indoor Court 1",
+        "surface": "hard",
+        "indoor": True
+    },
+    {
+        "name": "Indoor Court 2",
+        "surface": "hard",
+        "indoor": True
+    }
+]
--- main.py
+++ main.py
@@ -1,63 +1,68 @@
-court = {
-    "name": "Court 1",
-    "surface": "hard",
-    "indoor": False
-}
+from users import members, pros
+from courts import courts
+import json
 
-coach = {
-    "name": "Sean Given",
-    "user_type": "employee"
-}
+bookings_file = bookings.json
 
-member = {
-    "name": "John Smith",
-    "user_type": "member"
-}
+member = members[0]
+coach = pros[0]
+court = courts[0]
 
-OPEN_TIME = 8 * 60
-CLOSE_TIME = 20 * 60
-
-ALLOWED_DURATIONS = [30, 60, 90, 120]
+open_time = 8 * 60
+close_time = 20 * 60
+allowed_durations = [30, 60, 90, 120]
 
 
+#club number upon login determines the user booking the court
+
+def find_user_by_club_number(club_number):
+    all_users = pros + members
+    for user in all_users:
+        if user["club_number"] == club_number:
+            return user
+    return None
+club_number = int(input("Enter club number: "))
 
 
-reservation = {
-    "user": member,
-    "court": court,
-    "start_time": 10 * 60,
-    "duration": 60,
-}
 
-new_reservation = {
-    "user": coach,
-    "court": court,
-    "start_time": 10 * 60 + 30,
-    "duration": 60,
-}
 
-bookings = [reservation]
 
-### rule check functions
+current_user = find_user_by_club_number(club_number)
+if current_user is None:
+    print("User not found.")
+else:
+    print(f"Logged in as {current_user['first_name']} {current_user['last_name']}")
+
+##
+
+def load_bookings():
+    try:
+        with open(bookings_file, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+def save_bookings():
+    with open(BOOKING_FILE, "w") as file:
+        json.dump(bookings, file, indent=4)
+
+### rule check functions ----- #
 
 
 def is_valid_duration(duration):
-    return duration in ALLOWED_DURATIONS
-
-
+    return duration in allowed_durations
 
 def is_within_club_hours (start_time, duration):   #makes sure reservation is within
     end_time = start_time + duration
 
-    if start_time < OPEN_TIME:
+    if start_time < open_time:
         return False
     
-    if end_time > CLOSE_TIME:
+    if end_time > close_time:
         return False
     
 
     return True
-
 
 def bookings_overlap(new_booking, existing_booking):   #returns true or false
     new_start = new_booking["start_time"]
@@ -68,7 +73,6 @@ def bookings_overlap(new_booking, existing_booking):   #returns true or false
 
     return new_start < existing_end and new_end > existing_start
 
-
 def has_booking_conflict(new_booking, bookings):       #boolean
     for existing_booking in bookings:
         if bookings_overlap(new_booking, existing_booking):
@@ -76,8 +80,12 @@ def has_booking_conflict(new_booking, bookings):       #boolean
         
     return False
 
-            
-selected_user = member
+#--------#
+
+bookings = load_bookings()
+
+club_number = int(input("Enter club number: "))
+current_user = find_user_by_club_number(club_number)
 
 start_hour = int(input("Start hour: "))
 start_minute = int(input("Start minute: "))
@@ -86,7 +94,7 @@ duration = int(input("Duration: "))
 start_time = start_hour * 60 + start_minute
 
 new_reservation = {
-    "user": selected_user,
+    "user": current_user,
     "court": court,
     "start_time": start_time,
     "duration": duration,
@@ -101,4 +109,4 @@ elif has_booking_conflict(new_reservation, bookings):
 else:
     bookings.append(new_reservation)
     print("Reservation created.")
-
+    print(new_reservation)
